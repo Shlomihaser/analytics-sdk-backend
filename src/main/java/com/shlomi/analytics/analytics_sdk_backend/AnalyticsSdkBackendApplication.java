f88f573b8245e74359cef461310d2b0decc59e12
Shlomihaser <hasshlomi21@gmail.com>
2025-06-11 17:04:51 +0300

    Initial commit - backend API service with Spring Boot and MongoDB

---
+++ src/main/java/com/shlomi/analytics/analytics_sdk_backend/AnalyticsSdkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shlomi.analytics.analytics_sdk_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsSdkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsSdkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shlomi/analytics/analytics_sdk_backend/AnalyticsSdkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shlomi.analytics.analytics_sdk_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsSdkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
